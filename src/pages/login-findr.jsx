80599838c9d68837528090e4a09db637905f827a
Joao Lima <joao.lima@valemobi.com.br>
2022-05-22 16:31:05 -0300

    v5

--- src/pages/login-findr.jsx
+++ src/pages/login-findr.jsx
@@ -1,3 +1,4 @@
+import { render } from "@testing-library/react";
 import FindrButton from "../components/findr-button";
 import FindrInput from "../components/findr-input";
 import '../styles/findr-login-page-style.css';
@@ -11,20 +12,20 @@ function FindrLogin() {
             <div className="container-login-user">
                 <div className="saudacao-usuario">
                     <h1>Login</h1>
-                    <h3>Venha conectar-se em sua conta e venha usufruiu de
+                    <h3 className="text-greetings">Venha conectar-se em sua conta e venha usufruir de
                         nossos mehores produtos e análises</h3>
                     <FindrInput tooltip="abc" />
                     <FindrInput tooltip="abc" />
-                    <FindrButton label="login" />
+                    <FindrButton label="Login" className="button-login" onClick={() => this.handleClick()}/>
                         <div className="password-options">
-                            <a href="#">Esqueceu a senha?</a>
-                            <a href="#">Recuperar Senha</a>
+                            <a href="#" className="a-href-password">Esqueceu a senha?</a>
+                            <a href="#" className="a-href-password">Recuperar Senha</a>
                         </div>
                     <p className="also-login">Também faça Login com:</p>
                     <div className="login-options">
-                        <i className="pi pi-facebook" ></i>
-                        <i className="pi pi-twitter"  ></i>
-                        <i className="pi pi-google"   ></i>
+                        <i className="pi pi-facebook"></i>
+                        <i className="pi pi-twitter"></i>
+                        <i className="pi pi-google"></i>
                     </div>
                 </div>
             </div>
