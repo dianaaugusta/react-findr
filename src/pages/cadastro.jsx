c09a15f94c525f2a2b9d40ae0f3c0d1e880245da
Petterson Viturino <petterson.viturino@valemobi.com.br>
2022-05-26 21:37:42 -0300

    cadastro v1.03

--- src/pages/cadastro.jsx
+++ src/pages/cadastro.jsx
@@ -1,6 +1,7 @@
 import { InputText } from 'primereact/inputtext';
 import { Checkbox } from 'primereact/checkbox';
 import FindrInput from "../components/findr-input";
+import FindrButton from '../components/findr-button';
 import "../styles/findr-cadastro-style.css"
 
 
@@ -8,49 +9,28 @@ function Cadastro() {
     return (
         <>
             <div class="ajuste-tela">
-            <h1>Findr</h1>
-                <div class="imagem-cadastro">
+                <div className="container-content-form">
                     <img src="https://images.pexels.com/photos/3201585/pexels-photo-3201585.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                </div>
-                <div className='card-cadastro'>
-                    <div className='frases-cadastro'>
-                        Crie sua conta de freelancer e encontre projetos de <br /> forma rápida e prática
-                        <div class="formulario-cadastro">
-                            <div class="input-cadastro">
-                                <FindrInput tooltip="1" />
-                            </div>
-                            <div class="input-cadastro">
-                                <FindrInput tooltip="2" />
-                            </div>
-                            <div class="input-cadastro">
-                                <FindrInput tooltip="3" />
-                            </div>
-                            <div class="input-cadastro">
-                                <FindrInput tooltip="4" />
-                            </div>
-                            <div class="input-cadastro">
-                                <FindrInput tooltip="5" />
-                            </div>
-                            <div class="input-cadastro">
-                                <FindrInput tooltip="6" />
-                            </div>
-                            <div class="input-cadastro">
-                                <FindrInput tooltip="7" />
-                            </div>
-                            <div class="input-cadastro">
-                                <FindrInput tooltip="8" />
-                            </div>
-                            <div class="input-cadastro">
-                                <FindrInput tooltip="9" />
-                            </div>
-
+                    <div className="text-input">
+                        <p>Crie sua conta de freelancer e encontre projetos de forma rápida e prática</p>
+                    </div>
+                    
+                    <div className="container-inputs">
+                        <FindrInput/>
+                        <FindrInput/>
+                        <FindrInput/>
+                        <FindrInput/>
+                        <FindrInput/>
+                        <FindrInput/>
+                        <FindrInput/>
+                        <FindrInput/>
+                        <FindrInput/>
+                            <br />
+                        <div className="text-acceptance">
+                            <h3 className='i-acceptance'> Eu aceito os termos de uso</h3>        
+                                <FindrButton label="Cadastro"/>
+                                <h4 className='login-here'>Já tem conta? Faça o login <a href="#">aqui</a> </h4>
                         </div>
-                        <div class="check-box">
-                            <Checkbox inputId="cb1" value="New York"></Checkbox>
-                            <label htmlFor="cb1" class="p-checkbox-label"> Eu aceito os <a href="">termos</a> de uso</label>
-                        </div>
-                        <button>Cadastrar</button>
-                        <p>Já tem conta? Faça o login <a href="">aqui</a></p>
                     </div>
                 </div>
             </div>
