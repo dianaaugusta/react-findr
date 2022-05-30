0b9c5a0e702e8b67faea2ab67b9291908fcfee26
Petterson <petiviturino@gmail.com>
2022-05-30 20:20:32 -0300

    sla joao

--- src/pages/cadastro.jsx
+++ src/pages/cadastro.jsx
@@ -4,10 +4,10 @@ import FindrInput from "../components/findr-input";
 import FindrButton from '../components/findr-button';
 import { Link } from 'react-router-dom'
 import "../styles/findr-cadastro-style.css"
-import  {axios} from 'axios';
+import { axios } from 'axios';
 
-function postUser(){
-    
+function postUser() {
+
 };
 
 
@@ -21,22 +21,49 @@ function Cadastro() {
                     <div className="text-input">
                         <p>Crie sua conta de freelancer e encontre projetos de forma rápida e prática</p>
                     </div>
-                    
+
                     <div className="container-inputs">
-                        <FindrInput/>
-                        <FindrInput/>
-                        <FindrInput/>
-                        <FindrInput/>
-                        <FindrInput/>
-                        <FindrInput/>
-                        <FindrInput/>
-                        <FindrInput/>
-                        <FindrInput/>
-                            <br />
+                        <div class="individual-input-cadastro">
+                            <p>Nome:</p>
+                            <FindrInput id=""/>
+                        </div>
+                        <div class="individual-input-cadastro">
+                            <p>CPF:</p>
+                            <FindrInput id="" />
+                        </div>
+                        <div class="individual-input-cadastro">
+                            <p>E-mail:</p>
+                            <FindrInput id="" />
+                        </div>
+                        <div class="individual-input-cadastro">
+                            <p>Telelfone:</p>
+                            <FindrInput id="" />
+                        </div>
+                        <div class="individual-input-cadastro">
+                            <p>Senha:</p>
+                            <FindrInput id="" />
+                        </div>
+                        <div class="individual-input-cadastro">
+                            <p>Confirmar senha:</p>
+                            <FindrInput id="" />
+                        </div>
+                        <div class="individual-input-cadastro">
+                            <p>Estado:</p>
+                            <FindrInput id="" />
+                        </div>
+                        <div class="individual-input-cadastro">
+                            <p>Cidade</p>
+                            <FindrInput id="" />
+                        </div>
+                        <div class="individual-input-cadastro">
+                            <p>Nacionalidade</p>
+                            <FindrInput id="" />
+                        </div>
+                        <br />
                         <div className="text-acceptance">
-                            <h3 className='i-acceptance'> <input type="checkbox" /> Eu aceito os termos de uso</h3>        
-                                <FindrButton component={Link} to="localhost:8080/freelancer" onClick={() => postUser()} label="Cadastro"/>
-                                <h4 className='login-here'>Já tem conta? Faça o login <a href="#">aqui</a> </h4>
+                            <h3 className='i-acceptance'> <input type="checkbox" id="checkbox-accept" /> Eu aceito os termos de uso</h3>
+                            <FindrButton component={Link} to="localhost:8080/freelancer" onClick={() => postUser()} label="Cadastro" />
+                            <h4 className='login-here'>Já tem conta? Faça o login <a href="#">aqui</a> </h4>
                         </div>
                     </div>
                 </div>
