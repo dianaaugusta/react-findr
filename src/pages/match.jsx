3590b1f07a524a8546066f791d7079bad9be766c
ccojoaolima <joaolima80@outlook.com>
2022-08-25 21:32:15 -0300

    animacaozinha e tal mas peguei bug chato zz

--- src/pages/match.jsx
+++ src/pages/match.jsx
@@ -3,13 +3,14 @@ import FaqComponent from "../components/findr-faq-component";
 import FindrMenu from "../components/findr-menu"
 import '../styles/findr-match-style.css'
 import FindrBox from "../components/findr-box"
-
+import Fade from '@mui/material/Fade';
 function FindrMatch() {
     return (
         <>
             <FindrMenu/>
             <div class="card-match">
-                   <FindrBox/>
+                <FindrBox/>
+                 
             </div>
 
         </>
