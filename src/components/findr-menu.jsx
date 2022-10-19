8dfd409d9485d8db706535a087297d48a40b22f3
Gabriel Contardi <gabriel.contardi@valemobi.com>
2022-10-19 16:07:54 -0300

    FEAT

--- src/components/findr-menu.jsx
+++ src/components/findr-menu.jsx
@@ -1,4 +1,3 @@
-
 import { Menu } from 'primereact/menu';
 import '../styles/findr-menu-style.css'
 import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
@@ -8,8 +7,8 @@ import "primeicons/primeicons.css";
 function FindrMenu() {
 
     let items = [
-        {label: 'Perfil', icon: 'pi pi-fw pi-user', url: ''},
-        {label: 'Dashboards', icon: 'pi pi-fw pi-chart-line',  url: ''},
+        {label: 'Perfil', icon: 'pi pi-fw pi-user', command:()=>{ window.location.hash="/perfil";}},
+        {label: 'Dashboards', icon: 'pi pi-fw pi-chart-line',  url: 'http://localhost:3000/perfil'},
         {label: 'Conexões', icon: 'pi pi-fw pi-sitemap',  url: ''},
         {label: 'Matchs', icon: 'pi pi-fw pi-users',  url: ''},
         {label: 'Projetos', icon: 'pi pi-fw pi-id-card',  url: ''},
@@ -20,7 +19,7 @@ function FindrMenu() {
         <>
         <div class="container-menu">
         <div class="user-return-container">
-            <img src="https://thumbs.dreamstime.com/b/default-avatar-profile-flat-icon-social-media-user-vector-portrait-unknown-human-image-default-avatar-profile-flat-icon-184330869.jpg" alt="" />
+            <img src="https://i.pinimg.com/originals/12/5e/00/125e00eedea84514eaed380991657c44.jpg" alt="" />
            <h1>{sessionStorage.nomesuario}</h1>
         </div>
         <Menu model={items} className="menu-findr"/>
